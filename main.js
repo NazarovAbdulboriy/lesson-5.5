d2591f1b3898e3a34b2579481b69ba3226703b8b
NazarovAbdulboriy <nazarovabdulboriy377@gmail.com>
2024-04-25 08:40:48 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// const test = document.querySelector("#test");
+
+// test.style
+
+// const title = document.createElement("p")
+// const text = document.createTextNode("Hello Mars")
+// title.appendChild(text);
+// const main= document.querySelector("body");
+// main.appendChild(HelloMars)
+
+
+// const changeTitle =( ) => {
+//     const title = document.querySelector("h1")
+//     title.textContent = "Hello mars"
+// }
+
+// const buttonChange1 = document.getElementByld("btnChanger")
+
+const title = document.getElementsByClassName("test");
+title[0].innerHTML = prompt("Ismingizni kiriting..")
